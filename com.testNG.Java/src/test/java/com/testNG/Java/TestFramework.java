e52ab97963dd806ca0c033082f1beed24f02fd13
Akhil <Akhil@DESKTOP-JSAE176>
2024-08-17 15:13:50 +0530

    This is my demo push

---
+++ com.testNG.Java/src/test/java/com/testNG/Java/TestFramework.java
@@ -0,0 +1,20 @@
+package com.testNG.Java;
+
+import org.testng.annotations.Test;
+
+public class TestFramework {
+	@Test
+	public void c_Test() {
+		System.out.println("this is method from C");
+	}
+	@Test
+	public void a_Test() {
+		System.out.println("this is method from a");
+	}
+	@Test
+	public void b_Test() {
+		System.out.println("this is method from b");
+	}
+	
+
+}
